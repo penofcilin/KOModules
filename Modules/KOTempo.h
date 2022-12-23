87daa6398dcf331dfd53e7055d67a8576dd51ca1
Penofcilin <63890812+penofcilin@users.noreply.github.com>
2022-12-23 03:35:15 -0500

    Update KOTempo.h

--- Modules/KOTempo.h
+++ Modules/KOTempo.h
@@ -1,11 +1,9 @@
 /*
 ==============================================================================
-
 KOTempo.h
 Created: 26 Sep 2022 3:51:21am
 Author:  Penofcilin The King Of KO
-Purpose: Provides useful tempo related methods.
-
+Purpose: Provides useful tempo related methods, like getting the hertz values of notes
 ==============================================================================
 */
 
@@ -17,25 +15,20 @@ Purpose: Provides useful tempo related methods.
 class KOTempo
 {
 public:
+    enum NoteTypes {Whole, Half, DottedHalf, Quarter, DottedQuarter, TripletQuarter, Eighth, DottedEighth, TripletEighth, Sixteenth, DottedSixteenth, TripletSixteenth};
     void prepare(float bpm);
+    void prepare(float bpm, float samplerate);
     juce::StringArray getNoteTypes();
     juce::StringArray getStraightNoteTypes();
     juce::StringArray getModdedNoteTypes();
 
-    float getWholeNote();
-    float getHalfNote();
-    float getDottedHalfNote();
-    float getQuarterNote();
-    float getDottedQuarterNote();
-    float getTripletQuarterNote();
-    float getEighthNote();
-    float getDottedEighthNote();
-    float getTripletEighthNote();
-    float getSixteenthNote();
-    float getDottedSixteenthNote();
-    float getTripletSixteenthNote();
+    //These will return the length of a given note, either in hz, or samples
+    float getNoteLengthHertz(NoteTypes type);
+    float getNoteLengthSamples(NoteTypes type);
+    float getNoteLengthMS(NoteTypes type);
 
 private:
     float BPM;
+    float sampleRate;
 };
 #endif // !KOTempo_h

e92bd3dbf78545ce8808b3f3abcd6a0b679ae220
Penofcilin <63890812+penofcilin@users.noreply.github.com>
2022-12-25 01:30:51 -0500

    Finalized

--- Modules/KOTempo.h
+++ Modules/KOTempo.h
@@ -3,7 +3,20 @@
 KOTempo.h
 Created: 26 Sep 2022 3:51:21am
 Author:  Penofcilin The King Of KO
-Purpose: Provides useful tempo related methods, like getting the hertz values of notes
+Purpose: Provides useful tempo related methods, like getting the hertz values of notes. Make sure to set BPM and samplerate before using. 
+
+
+hint you can easily find bpm in process block with following code:
+
+(in processor header file)
+KOTempo tempo;
+juce::AudioPlayHead* playHead;
+juce::AudioPlayHead::CurrentPositionInfo currentPosition;
+
+(In processblock)
+playHead = this->getPlayHead();
+playHead->getCurrentPosition(currentPos);
+tempo.setBPM(currentPos.bpm);
 ==============================================================================
 */
 
@@ -13,23 +26,22 @@ Purpose: Provides useful tempo related methods, like getting the hertz values of
 
 #include <JuceHeader.h>
 
-//You can use this macro to easily add all the options to an apvts choice parameter.
-#ifndef NOTE_TYPES_STRINGS 
+    #ifndef NOTE_TYPES_STRINGS 
     #define NOTE_TYPES_STRINGS  "Whole", "Half", "Dotted Half", "Quarter", "Dotted Quarter", "Triplet Quarter", "Eighth", "Dotted Eighth", "Triplet Eighth", "Sixteenth", "Dotted Sixteenth", "Triplet Sixteenth"
     #endif // !NOTE_TYPES_STRINGS 
+
 
 class KOTempo
 {
 public:
     enum NoteTypes {Whole, Half, DottedHalf, Quarter, DottedQuarter, TripletQuarter, Eighth, DottedEighth, TripletEighth, Sixteenth, DottedSixteenth, TripletSixteenth};
-    void prepare(float bpm);
-    void prepare(float bpm, float samplerate);
-    //Dont use these for declaring an APVTS parameter
+    void setBPM(float bpm);
+    void setSampleRate(float samplerate);
     juce::StringArray getNoteTypes();
     juce::StringArray getStraightNoteTypes();
     juce::StringArray getModdedNoteTypes();
 
-    //These will return the length of a given note, either in hz, or samples
+    //These will return the length of a given note, either in hz, samples or milliseconds
     float getNoteLengthHertz(NoteTypes type);
     float getNoteLengthSamples(NoteTypes type);
     float getNoteLengthMS(NoteTypes type);

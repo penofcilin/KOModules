3c29a6c7e3dc6748a20a90fff3ae95a651e25d67
Penofcilin <63890812+penofcilin@users.noreply.github.com>
2022-12-25 01:25:01 -0500

    Added notes and macro

--- Modules/KOTempo.h
+++ Modules/KOTempo.h
@@ -12,12 +12,19 @@ Purpose: Provides useful tempo related methods, like getting the hertz values of
 #define KOTempo_h
 
 #include <JuceHeader.h>
+
+//You can use this macro to easily add all the options to an apvts choice parameter.
+#ifndef NOTE_TYPES_STRINGS 
+    #define NOTE_TYPES_STRINGS  "Whole", "Half", "Dotted Half", "Quarter", "Dotted Quarter", "Triplet Quarter", "Eighth", "Dotted Eighth", "Triplet Eighth", "Sixteenth", "Dotted Sixteenth", "Triplet Sixteenth"
+    #endif // !NOTE_TYPES_STRINGS 
+
 class KOTempo
 {
 public:
     enum NoteTypes {Whole, Half, DottedHalf, Quarter, DottedQuarter, TripletQuarter, Eighth, DottedEighth, TripletEighth, Sixteenth, DottedSixteenth, TripletSixteenth};
     void prepare(float bpm);
     void prepare(float bpm, float samplerate);
+    //Dont use these for declaring an APVTS parameter
     juce::StringArray getNoteTypes();
     juce::StringArray getStraightNoteTypes();
     juce::StringArray getModdedNoteTypes();
